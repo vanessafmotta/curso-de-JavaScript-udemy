76c62326833a7b388d0b9212fb64ed4a633d110e
Vanessa Motta <89320692+vanessafmotta@users.noreply.github.com>
2022-04-01 22:07:40 -0300

    funcao-adicionar-email

--- DOM_saudacao/DOM_newsletter_original/js/newsletter.js
+++ DOM_saudacao/DOM_newsletter_original/js/newsletter.js
@@ -1,8 +1,8 @@
 const txtEmail = document.getElementById("txtEmail")
-const msgFeedback = document.getElementById("msgFeedback")
+const msgFeedback = document.getElementById("newsletterFeedback")
 
 function cadastrarEmail(){
     let email = txtEmail.value
     msgFeedback.innerHTML = `O ${email} foi cadastrado com sucesso!`
-    console.log(cadastrarEmail);
+    
 }
